7b9ab9f04e04ed54e0ccf0294c3572d17d2d6c01
Brendo Raposo <raposobrendo@gmail.com>
2023-10-13 16:10:58 -0300

    Primeiro commit

---
+++ src/main/java/br/com/raposobrendo/todolist/user/UserModel.java
@@ -0,0 +1,12 @@
+package br.com.raposobrendo.todolist.user;
+
+
+/**
+ * Body
+ */
+public class UserModel {
+    
+    private String username;
+    private String name;
+    private String password;
+}
