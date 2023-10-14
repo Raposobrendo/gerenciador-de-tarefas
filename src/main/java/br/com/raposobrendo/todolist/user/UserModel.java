8d65fe31805e6754657b77a3e9e8f5f85d030100
Brendo Raposo <raposobrendo@gmail.com>
2023-10-14 03:00:51 -0300

    Inserção do uso de getters e setters + lombok

--- src/main/java/br/com/raposobrendo/todolist/user/UserModel.java
+++ src/main/java/br/com/raposobrendo/todolist/user/UserModel.java
@@ -1,12 +1,15 @@
 package br.com.raposobrendo.todolist.user;
 
+import lombok.Data;
 
 /**
  * Body
  */
+@Data
 public class UserModel {
     
     private String username;
     private String name;
     private String password;
+
 }
